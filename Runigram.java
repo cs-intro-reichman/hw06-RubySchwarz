b12bd8a65c125c7c04de25c66c25af096ed22a3c
Ruby Schvartz <ruby.schvartz@runi.ac.il>
2024-01-29 15:53:03 +0200

    Add code for Homework 6 again2

--- Runigram.java
+++ Runigram.java
@@ -6,39 +6,48 @@ import java.util.Scanner;
 public class Runigram {
 
     public static Color[][] read(String filename) {
-        // Existing implementation
+        // Existing implementation for reading a PPM file
     }
 
     public static void print(Color[][] image) {
-        // Existing implementation
+        // Existing implementation for printing image details
     }
 
-    // Renamed to match the test cases
     public static Color[][] flippedHorizontally(Color[][] image) {
         // Implementation of horizontal flipping
     }
 
-    // Renamed to match the test cases
     public static Color[][] flippedVertically(Color[][] image) {
         // Implementation of vertical flipping
     }
 
-    // Renamed to match the test cases
     public static Color[][] grayScaled(Color[][] image) {
         // Implementation of grayscale conversion
     }
 
-    // New method: luminance
     public static Color luminance(Color color) {
         // Implementation of luminance calculation
     }
 
-    // New method: blend
     public static Color[][] blend(Color[][] image1, Color[][] image2, double alpha) {
         // Implementation of blending two images
     }
 
-    // Methods for scaling and morphing (as needed)
+    public static Color[][] scaled(Color[][] image, int newWidth, int newHeight) {
+        int width = image[0].length;
+        int height = image.length;
+        Color[][] newImage = new Color[newHeight][newWidth];
+
+        for (int y = 0; y < newHeight; y++) {
+            for (int x = 0; x < newWidth; x++) {
+                int srcX = x * width / newWidth;
+                int srcY = y * height / newHeight;
+                newImage[y][x] = image[srcY][srcX];
+            }
+        }
+
+        return newImage;
+    }
 
     public static void main(String[] args) {
         // Testing the functions as per your requirements
